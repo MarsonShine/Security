51efb6fea8eb07389ac640879e5ed9de4ba547b1
marsonshine <ms27946@163.com>
2021-01-18 15:21:29 +0800

    feat: 密码学安全伪随机生成数 慢哈希函数存储密码，被拖库也无法短时间内破解密文

---
+++ BCrypt/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+using static System.Console;
+
+namespace BCrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WriteLine("按 ESC 退出...");
+            while (ReadKey().Key != ConsoleKey.Escape)
+            {
+                CSPRNG();
+            }
+            WriteLine("退出...");
+            BCryptUsage();
+            BCryptUsage2();
+
+
+            ReadLine();
+        }
+
+        static void CSPRNG()
+        {
+            RandomNumberGenerator randomNumberGenerator = new RNGCryptoServiceProvider();
+            byte[] rawByteArry = new byte[16];
+            randomNumberGenerator.GetBytes(rawByteArry);
+            WriteLine(string.Join(',', rawByteArry));
+        }
+
+        static void BCryptUsage()
+        {
+            // 前端传的密码
+            string password = "marsonshine123qwe";
+            // 利用RNG生成salt
+            string salt = Net.BCrypt.GenerateSalt(10, 'a');
+            // 慢哈希加密
+            string client_bhash = Net.BCrypt.HashPassword(password, salt);
+
+            WriteLine(client_bhash);
+
+        }
+
+        //验证
+        static void BCryptUsage2()
+        {
+            string client_bash = "$2a$10$.rAIuzCI4hBVECwjAbj3xeH6GEmYnL.xdm.gct6yj568EEIP2feKO";
+            WriteLine(Net.BCrypt.Verify("marsonshine123qwe", client_bash) ? "验证成功" : "验证失败");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Security
 关于安全的一些零碎的知识整理
+
+## 慢哈希（BCrypt）
+
+概念介绍：https://coolshell.cn/articles/2078.html
+
+慢哈希函数是指这个函数的执行事件是可以调节的哈希函数，它通常是可以调节调用次数来实现的。
+
+.NET 的实现库地址：https://github.com/BcryptNet/bcrypt.net
+
+我们在为哈希选择盐值时，不要选系统伪随机生成树，而是选择基于密码学的伪随机生成数，这称为 “密码学安全伪随机数”（Cryptographically Secure Pseudo-Random Number Generator）来生成一个长度与哈希值相等的随机字符串。
